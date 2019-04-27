e58ef51ba96987fdc0698cbc8520f7a77c2de388
tweeddale@gmail.com <tweeddale@gmail.com>
2019-04-27 08:56:49 -0400

    auto-refresh latest picture

--- ui/src/app/station-card/station-card.component.ts
+++ ui/src/app/station-card/station-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ViewChild, AfterViewInit, Output, EventEmitter } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'station-card',
@@ -17,8 +18,15 @@ export class StationCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.latestImgPath = environment.hostUrlBase +"/station-images/"+ this.station.uuid + "/latest.jpg";
+        this.updatePic();
+        timer(0, 10000).subscribe(() => this.updatePic());
   }
+
+  updatePic(){
+    var randomNum: number = Math.floor(Math.random() * 1000000) + 1;
+    this.latestImgPath = environment.hostUrlBase + "/station-images/"+ this.station.uuid + "/latest.jpg?rand=" + randomNum;
+  }
+
 
   touchUpdateTime(lastUpdateTime: Date) {
     this.readingsLastUpdated = lastUpdateTime;
