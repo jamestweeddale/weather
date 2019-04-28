746f8477f3bb626cabb42ca83ce649acb3b9fab1
tweeddale@gmail.com <tweeddale@gmail.com>
2019-04-28 14:30:46 -0400

    fix unit test

--- station/src/test/java/station/io/convert/MCP3008OutputConverterTest.java
+++ station/src/test/java/station/io/convert/MCP3008OutputConverterTest.java
@@ -10,7 +10,7 @@ public class MCP3008OutputConverterTest {
 
     @Test
     public void test_anemometer_value_conversion_min(){
-        MCP3008OutputConverter adafruitAnemometerConverter = new MCP3008OutputConverter(AdafruitAnemometer.SENSOR_VOLTAGE_MIN, AdafruitAnemometer.SENSOR_VOLTAGE_MAX, AdafruitAnemometer.MAX_WIND_SPEED_MPS);
+        MCP3008OutputConverter adafruitAnemometerConverter = new MCP3008OutputConverter(AdafruitAnemometer.SENSOR_VOLTAGE_MIN, AdafruitAnemometer.SENSOR_VOLTAGE_MAX, AdafruitAnemometer.MAX_WIND_SPEED_MPS, 0);
 
         //.4v is min output of adafruit anemometer sensor (0 mps)
         double result = adafruitAnemometerConverter.convert(.4 / MCP3008OutputConverter.VOLTAGE_CONVERSION_CONSTANT);
@@ -21,7 +21,7 @@ public class MCP3008OutputConverterTest {
 
     @Test
     public void test_anemometer_value_conversion_max(){
-        MCP3008OutputConverter adafruitAnemometerConverter = new MCP3008OutputConverter(AdafruitAnemometer.SENSOR_VOLTAGE_MIN, AdafruitAnemometer.SENSOR_VOLTAGE_MAX, AdafruitAnemometer.MAX_WIND_SPEED_MPS);
+        MCP3008OutputConverter adafruitAnemometerConverter = new MCP3008OutputConverter(AdafruitAnemometer.SENSOR_VOLTAGE_MIN, AdafruitAnemometer.SENSOR_VOLTAGE_MAX, AdafruitAnemometer.MAX_WIND_SPEED_MPS, 0);
 
         //2v is max output of adafruit anemometer sensor (32.4 mps)
         double result = adafruitAnemometerConverter.convert(2.0 / MCP3008OutputConverter.VOLTAGE_CONVERSION_CONSTANT);
